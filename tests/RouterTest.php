45505f0a613ec2ea02f4bff601e36077932ad2e0
Auios <LoneAuios@gmail.com>
2023-06-25 17:50:57 -0400

    Unit test start

---
+++ tests/RouterTest.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+use BadRouter\Router;
+
+class RouterTest extends TestCase {
+  public function testCanBeCreatedFromValidEmail(): void {
+    // Router::run();
+
+    $this->assertSame('aaa', 'baaa');
+  }
+}
